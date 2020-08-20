1135ec358211e1734b52eb4dfe1a0867e3b4e79a
nkalyesubula <noahkalyesubula@gmail.com>
2020-08-20 02:08:01 -0700

    Generate composer.lock file

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -10,15 +10,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- full calendar css -->
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{URL:: asset('css/plugins/fontawesome-free/css/all.min.css') }}">
+   <!-- <! <link rel="stylesheet" href="{{URL:: asset('css/plugins/fontawesome-free/css/all.min.css') }}"> -->
     <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
     <!-- Tempusdominus Bbootstrap 4 -->
-    <link rel="stylesheet" href="{{URL:: asset('css/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+    <!-- <link rel="stylesheet" href="{{URL:: asset('css/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}"> -->
     <!-- DataTables -->
-    <link rel="stylesheet" href="{{URL:: asset('css/bootstrap.css') }}">
+    <!-- <link rel="stylesheet" href="{{URL:: asset('css/bootstrap.css') }}"> -->
     <!--Custom css -->
-    <link rel="stylesheet" href="{{URL:: asset('css/rentals.css') }}">
+    <!-- <link rel="stylesheet" href="{{URL:: asset('css/rentals.css') }}"> -->
     <!--Custom css -->
     <link rel="stylesheet" href="{{URL:: asset('css/dataTables.bootstrap4.min.css') }}">
     <!-- SweetAlert2 -->
@@ -32,4 +32,18 @@
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
